68a1328d6e60adebd682d9daaa775cc848e01e59
Chad Banicki <cbanicki@homeaway.com>
2016-08-09 16:57:29 -0500

    made quite a few changes, as well as added a slidify document

--- server.R
+++ server.R
@@ -2,26 +2,94 @@ library(shiny)
 library(datasets)
 library(dplyr) 
 library(wordcloud)
+library(ggplot2)
 
+options(scipen=5)
+#setwd("C:\\R\\DevDataProd\\shinyapp")
+  dataset <- read.csv("datasetTrim.csv", header = TRUE,na.strings = c("NA","#DIV/0!",""))
 
-  dataset <- read.csv("NationalNames.csv", header = TRUE,na.strings = c("NA","#DIV/0!",""))
- #browser()
+  #Did this cleanup on the initial dataset and then saved it to the source to avoid having to do these each time 
+  #Find the popularity of each name over time and remove those with less than 10,000
+  
+  #dataset <- read.csv("NationalNames.csv", header = TRUE,na.strings = c("NA","#DIV/0!",""))
+  
+  
+#   dataTrim <- as.data.frame(
+#     dataset %>%
+#       group_by(Name) %>%
+#       summarize(POPULARITY = sum(Count))) #%>%
+#     filter(POPULARITY >= 10000)) 
+  
+  #replace original dataset with trimmed one containing only popular names
+  # dataset <- dataset[(dataset$Name %in%  dataTrim$Name),]
+  
+  #Take the top 50 for later analysis
+  topAllNames <- 
+    dataset %>%
+    group_by(Name,Gender) %>%
+    summarize(POPULARITY = sum(Count))
+  
+  
+  topTFNames <- topAllNames[order(-topAllNames$POPULARITY),][1:25,]
+  
+  #Get the data for the top All
+  topAll <- subset(dataset, Name %in% topTFNames$Name)
+  
+  
 
   shinyServer(function(input, output) {
+  
+  #topNames <- dataTrim[order(-dataTrim$POPULARITY),][1:25,]
 
   output$caption <- renderText({
     input$caption
   })
   
+ output$big_bar <- renderPlot({  
+   
+   
+   topNames <- 
+     dataset %>%
+     filter(
+       Year >= as.numeric(input$Range[1]),
+       Year <= as.numeric(input$Range[2])) %>%
+     group_by(Name,Gender) %>%
+     summarize(POPULARITY = sum(Count))
+ 
 
-  observeEvent(input$update, {
+ topNames <- topNames[order(-topNames$POPULARITY),][1:as.numeric(input$obs),]
     
+    barplot(topNames$POPULARITY/1000000, las = 2, names.arg = topNames$Name,
+            col =  ifelse(topNames$Gender=='M','blue','pink'), main ="Baby Names",
+            ylab = "Count (Millions)")
+    
+  })
+  
 
+#   observeEvent(input$update, {
+#     
+# 
+#  
+#     })
+    
+    
+  # Make the wordcloud drawing predictable during a session
+  wordcloud_rep <- repeatable(wordcloud)
+    
+  output$make_cloud <- renderPlot({  
+      
+    
+#     output$topBox <- renderValueBox({
+#       valueBox(
+#         input$obs, "Top Names", icon = icon("fa fa-child"),
+#         color =  ifelse(input$Gender=='M','light-blue','fuchsia')
+#       )
+      
     dataSum <- as.data.frame(
       dataset %>%
-        filter(dataset$Year >= as.numeric(input$Range[1]),
-               dataset$Year <= as.numeric(input$Range[2]),
-               dataset$Gender == input$Gender) %>%
+        filter(Year >= as.numeric(input$Range[1]),
+               Year <= as.numeric(input$Range[2]),
+               Gender == input$Gender) %>%
         group_by(Name) %>%
         summarize(POPULARITY = sum(Count)) %>%
         select(Name,POPULARITY))
@@ -30,11 +98,7 @@ library(wordcloud)
     dataSum <- head(dataSum[order(-dataSum$POPULARITY),],as.numeric(input$obs)) 
     
     
-  # Make the wordcloud drawing predictable during a session
-  wordcloud_rep <- repeatable(wordcloud)
     
-  output$make_cloud <- renderPlot({  
-      
     wordcloud_rep(dataSum$Name,dataSum$POPULARITY,
                         scale=c(4, 0.5),
                          min.freq=1,
@@ -42,12 +106,76 @@ library(wordcloud)
                          random.order = FALSE,
                          rot.per=0.35,
                          use.r.layout=TRUE,
-                         colors=brewer.pal(8, "Dark2"), 
+                         colors=brewer.pal(8, "Set1"), 
                          main="Common Baby Names")
 
-        })
-    
+        # })
+  
+
+  
   })
 
-})
+  
+  # observeEvent(input$updateSearch, {
+    
+  
+    
+    # output$topCount <-dataName[order(-dataName$Count),][1,2]
+    
+# 
+#     dataName <- as.data.frame(
+#       dataset %>%
+#         filter(dataset$Name == "Mary",
+#                dataset$Year >= 1880,
+#                dataset$Year <= 1930) %>%
+#         group_by(Year) %>%
+#         summarise(Count = sum(Count))) %>%
+#    select(Year,Count)
+    
+    output$distPlot <- renderPlot({
+
+      dataName <- as.data.frame(
+        dataset%>%
+          filter(tolower(Name) == tolower(input$name),
+                 Year >= as.numeric(input$Range[1]),
+                 Year <= as.numeric(input$Range[2])) %>%
+          group_by(Year) %>%
+          summarise(Count = sum(Count)))  %>%
+        select(Year,Count)
+      
+      topYear <- as.character(dataName[order(-dataName$Count),][1,1])
+      
+      output$topYear <- renderValueBox({
+        valueBox(
+          topYear, "Top Year", icon = icon("fa fa-child"),
+          color =  'purple'
+        )
+      }) 
+      
+      
+      
+      require(ggplot2)
+      
+      p <- ggplot(dataName, aes(Year,Count)) 
+      p <- p + geom_bar(stat="identity")   
+      p <-p + theme(axis.text.x=element_text(angle=45, hjust=1, size=14))   
+      p
+      
+
+    })
+  
+
+
+#     
+#     })
+    
+
+
+  })
+  
+
+    
+
+    
+
 
--- ui.R
+++ ui.R
@@ -1,37 +1,98 @@
+## ui.R ##
+
 library(shiny)
+library(shinydashboard)
 
-#setwd("C:\\R\\DevDataProd\\shinyapp")
 
-# Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
+sidebar <- dashboardSidebar(
   
-  # Application title
-  headerPanel("Baby Names"),
+    tags$hr(),
   
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-    selectInput("Gender", "Choose a Gender:",
-                choices=c("M","F")),
+    sliderInput("Range", label = "Birth Years:",min = 1880, max = 2014, value = c(1880,2014), sep = ""),
+      
+    tags$hr(),
     
-    sliderInput("Range", label = "Range:",min = 1880, max = 2014, value = c(1880,2014)),
-
-
-    numericInput("obs", "Number of Names to View:", value = 10, min = 1, max = 100),
-    
-    
-    actionButton("update", "Update Graph")
-    
-  ),
+        sidebarMenu(
+          
+              menuItem("Instructions", tabName = "main",
+                     badgeLabel = "Select View", icon = icon("fa fa-birthday-cake")),
+          
+              tags$hr(),
+          
+              menuItem("Popular Names", icon = icon("fa fa-pie-chart"), tabName = "dashboard",
+                       badgeLabel = "Select View", badgeColor = "green"),
+          
+                  fluidRow(
+                    
+                    column(6,
+                 
+                           selectInput("Gender", "Gender:",
+                              choices=c("M","F"), width = '100px')),
+                    
+                    column(6,
+                           
+                            numericInput("obs", "# Names:", value = 10, min = 1, max = 100, width = '100px'))
+                  
+                  ),
+          
+                tags$hr(),
+          
+                menuItem("Timeline", icon = icon("fa fa-line-chart"), tabName = "widgets",
+                         
+                          badgeLabel = "Select View", badgeColor = "green"),
+                
+                          textInput("name", "Name:", value = "", width = '400px', placeholder = "ex. Jim")
+                ),
+          
+              tags$hr()
   
-
-  # Show the caption, a summary of the dataset and an HTML table with
-  # the requested number of observations
-  mainPanel(
-    h3(textOutput("caption")), 
-    
-    plotOutput("make_cloud")
-
   )
+
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "main", 
+            # Dynamic valueBoxes
+            fluidRow(
+              column(3,img(src='baby.png', height = 72, width = 100, align = "left")),
+              column(9,h2("Popular US Baby Names (1880-2014)"),
+                     tags$ul(
+                       tags$li("Select a range of birth years to filter by"), 
+                       tags$li("Click on 'Select View' to view each chart"), 
+                       tags$li("Use the filters to change the gender, and the topN popular names"),
+                       tags$li("Type a name to view the popularity of that name over time")
+                     )),
+              h5("US Baby Names Dataset", a("Link", href="https://www.kaggle.com/kaggle/us-baby-names")), 
+              plotOutput("big_bar")
+            )
+    ),
+    tabItem(tabName = "dashboard", 
+            # Dynamic valueBoxes
+            fluidRow(
+            #column(3,img(src='baby.png', height = 72, width = 100, align = "left")),
+            column(7,h2("Most Popular Baby Names"))
+            ),
+            # valueBoxOutput("topBox"),
+            plotOutput("make_cloud")
+    ),
+    
+    tabItem(tabName = "widgets",
+            valueBoxOutput("topYear"),
+            h2("Baby Name Popularity Over Time"),
+            #img(src='baby.png', align = "right"),
+            plotOutput("distPlot")
+    )
+  )
+)
+
+# Put them together into a dashboardPage
+dashboardPage(
+  dashboardHeader(title = "US Baby Names"),
+  sidebar,
+  body
   
-))
+
+)
+
+
+
